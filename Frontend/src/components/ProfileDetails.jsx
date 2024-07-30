2e2e2a13e2619d861b5470b02b82751c7c6cbab3
sam1132 <sam113273@gmail.com>
2024-07-30 09:41:10 +0530

    put request fixed

--- Frontend/src/components/ProfileDetails.jsx
+++ Frontend/src/components/ProfileDetails.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { Button, Label, TextInput } from "flowbite-react";
 import toast from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Profile = () => {
-  const [formData,setFormData]=useState({
-    address: '',
-    city: '',
-    landMark: '',
-    phoneNo: '',
-    pincode: '',
-  })
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    address: "",
+    city: "",
+    landMark: "",
+    phoneNo: "",
+    pincode: "",
+  });
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -16,12 +19,12 @@ const Profile = () => {
       [name]: value,
     }));
   };
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     const { address, city, landMark, phoneNo, pincode } = formData;
 
     if (!address || !city || !landMark || !phoneNo || !pincode) {
-      toast.error("All Field are required")
+      toast.error("All Field are required");
       return;
     }
     if (!/^\d{10}$/.test(phoneNo)) {
@@ -29,19 +32,57 @@ const Profile = () => {
       return;
     }
     if (!/^\d{6}$/.test(pincode)) {
-      toast.error('Pincode must be a 6-digit number.');
+      toast.error("Pincode must be a 6-digit number.");
       return;
     }
-    console.log('Form submitted:', formData);
-  }
+    const updateData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          toast.error("Login again");
+          navigate("/signin");
+        }
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const response = await axios.put(
+          "http://localhost:4000/user/completeProfile",
+          formData,
+          config
+        );
+        toast.success("Profile updated successfully");
+        setFormData({
+          username: "",
+          email: "",
+          address: "",
+          city: "",
+          landMark: "",
+          phoneNo: "",
+          pincode: "",
+        });
+        navigate("/profile");
+      } catch (error) {
+        console.log(error)
+        toast.error("Cannot update profile")
+      }
+    };
+    updateData();
+  };
   return (
     <>
       <div className="flex flex-col justify-center items-center h-[100vh] m-7 px-4 md:px-6 lg:px-8 my-10 ">
-        <h1 className="text-center text-3xl mb-2 text-slate-900 font-bold ">Fill Out Your Details</h1>
-        <form className="flex max-w-md flex-col gap-4 shadow-lg px-10 w-[600px]" onSubmit={handleSubmit}>
+        <h1 className="text-center text-3xl mb-2 text-slate-900 font-bold ">
+          Fill Out Your Details
+        </h1>
+        <form
+          className="flex max-w-md flex-col gap-4 shadow-lg px-10 w-[600px]"
+          onSubmit={handleSubmit}
+        >
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="address" value="Your Address"/>
+              <Label htmlFor="address" value="Your Address" />
             </div>
             <TextInput
               id="address"
@@ -50,12 +91,11 @@ const Profile = () => {
               value={formData.address}
               onChange={handleChange}
               placeholder="Your address"
-              
             />
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="city" value="Your City"/>
+              <Label htmlFor="city" value="Your City" />
             </div>
             <TextInput
               id="city"
@@ -64,12 +104,11 @@ const Profile = () => {
               value={formData.city}
               onChange={handleChange}
               placeholder="city"
-              
             />
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="landMark" value="Landmark"/>
+              <Label htmlFor="landMark" value="Landmark" />
             </div>
             <TextInput
               id="landMark"
@@ -82,7 +121,7 @@ const Profile = () => {
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="phoneNo" value="Phone No"/>
+              <Label htmlFor="phoneNo" value="Phone No" />
             </div>
             <TextInput
               id="phoneNo"
@@ -91,12 +130,12 @@ const Profile = () => {
               onChange={handleChange}
               type="tel"
               placeholder="phone no"
-              maxLength='10'
+              maxLength="10"
             />
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="pincode" value="Pincode"/>
+              <Label htmlFor="pincode" value="Pincode" />
             </div>
             <TextInput
               id="pincode"
@@ -108,7 +147,12 @@ const Profile = () => {
               maxLength="6"
             />
           </div>
-          <Button type="submit"  className="text-white bg-blue-600 hover:scale-105 transition-all delay-150 my-2 font-bold">Submit</Button>
+          <Button
+            type="submit"
+            className="text-white bg-blue-600 hover:scale-105 transition-all delay-150 my-2 font-bold"
+          >
+            Submit
+          </Button>
         </form>
       </div>
     </>
