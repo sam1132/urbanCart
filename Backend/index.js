65c5b7df352defe3fb9ea00e9c6630be0b5d4ccd
sam1132 <sam113273@gmail.com>
2024-08-22 00:02:16 +0530

    correct cors

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ connectDb();
 //routes
 
 app.use(
-  cors({ origin: "https://urban-cart-eight.vercel.app/", credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  cors({ origin: "https://urban-cart-eight.vercel.app", credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: [ 'Authorization'], })
 );
 app.use(express.json());
