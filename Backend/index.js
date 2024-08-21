9f4144bc2a3f6e23a4bfa6fe9af95dc5e2e2f970
sam1132 <sam113273@gmail.com>
2024-08-22 00:03:47 +0530

    removed headers key from cors

--- Backend/index.js
+++ Backend/index.js
@@ -19,8 +19,7 @@ connectDb();
 //routes
 
 app.use(
-  cors({ origin: "https://urban-cart-eight.vercel.app", credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: [ 'Authorization'], })
+  cors({ origin: "https://urban-cart-eight.vercel.app", credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE']})
 );
 app.use(express.json());
 app.use(
