14f3bbee3288494f04e709d19ca1d5fe6eae8f2e
Sameer <117072712+sam1132@users.noreply.github.com>
2025-07-26 13:31:22 +0530

    Update connectdb.js

--- Backend/db/connectdb.js
+++ Backend/db/connectdb.js
@@ -6,9 +6,7 @@ const URI = process.env.MONGODB_URI
 const connectDb = async()=>{
    try {
       const connectionInstance =  await mongoose.connect(URI)
-      console.log(`MongoDB connected host : ${connectionInstance.connection.host}`)
    } catch (error) {
-    console.log("Mongodb connection error",error)
     process.exit(1)
    } 
 }
